59f8fcfce6b2f62641bbc315ffa8709b10f617a7
Daniels <voiceofwise1@gmail.com>
2021-08-18 17:01:14 +0300

    Make addresstoPhysical a static function

--- cepums/MemoryManager.h
+++ cepums/MemoryManager.h
@@ -16,7 +16,7 @@ namespace Cepums {
         uint16_t readWord(uint16_t segment, uint16_t offset);
         void writeWord(uint16_t segment, uint16_t offset, uint16_t value);
 
-        uint32_t addresstoPhysical(const uint16_t& segment, const uint16_t& offset);
+        static uint32_t addresstoPhysical(const uint16_t& segment, const uint16_t& offset);
         std::pair<uint16_t, uint16_t> addressToLogical(const uint32_t& physicalAddress);
     private:
         std::vector<uint8_t> m_RAM;
